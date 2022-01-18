87724728f9edacfa7a16c30b7897e65107f098b7
worapit <drm.wmwork@gmail.com>
2022-01-18 15:33:01 +0700

    complete

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -32,3 +32,46 @@ int main(){
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
 }
+
+void randData(double *a,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        *(a + i) = (rand()%101)*0.01;
+    }
+}
+
+void showData(double *a,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << *(a + i);
+        if((i+1)%M == 0)cout << endl;
+        else cout << " ";
+    }
+}
+
+void findRowSum(const double *a,double *sum1,int N,int M){
+	int j = 0,i = 0;
+	double num = 0.0;
+	while(j < N*M){
+		num = 0;
+		for(int k = 0;k < M;k++){
+			num += *(a + j);
+			j++;
+		}
+		sum1[i] = num;
+		i++;
+	}
+}
+
+void findColSum(const double *a,double *sum2,int N,int M){
+	int j = 0;
+	double num = 0.0;
+	while(j < M){
+	    num = 0;
+		for(int k = 0; k < N;k++){
+			num += *(a + j + k*M);
+		}
+		sum2[j] = num;
+		j++;
+	}
+	
+}
